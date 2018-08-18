11084020481b9961882a1c3b4eb21d44cae53f72
tacyarg <jdent@tacyarg.com>
2018-08-17 23:09:19 -0500

    typos

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -5,11 +5,10 @@ class UserMenu extends Component {
   render() {
 
     const {auth} = this.props
-
     return (
       <Menu
-        // large={true}
-      >
+          // large={true}
+        >
         <Menu.Item text="Profile" icon="user" />
         <Menu.Item text="Settings" icon="cog" />
         <Menu.Item text="My Cases" icon="box" />
