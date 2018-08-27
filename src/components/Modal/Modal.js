ff975d225b0cea3414f6a5cf5d9a5e9cf50fe431
tacyarg <jdent@tacyarg.com>
2018-08-27 14:02:23 -0500

    modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import classNames from "classnames";
+
+import "./Modal.css";
+import { Classes, Button, Overlay } from "@blueprintjs/core";
+import Profile from "../Modals/Profile/Profile";
+
+class Modal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: true,
+      modal: "Profile"
+    };
+  }
+
+  toggleOverlay() {
+    this.state.isOpen
+      ? this.setState({ isOpen: false })
+      : this.setState({ isOpen: true });
+  }
+
+  render() {
+    const classes = classNames(
+      Classes.CARD,
+      Classes.ELEVATION_4,
+      "Modal-overlay"
+    );
+    var { user } = this.props;
+    return (
+      <div className="Modal-wrapper">
+        <Button
+          style={{ float: "right" }}
+          large={true}
+          text="Show overlay"
+          onClick={this.toggleOverlay.bind(this)}
+        />
+        <Overlay
+          isOpen={this.state.isOpen}
+          onClose={this.toggleOverlay.bind(this)}
+        >
+          <div className={classes}>
+            <Profile user={user} />
+          </div>
+        </Overlay>
+      </div>
+    );
+  }
+}
+
+export default Modal;
---
+++ src/components/Modals/Profile/Profile.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import classNames from "classnames";
+
+import "./Profile.css";
+import { Tab, Tabs, Label } from "@blueprintjs/core";
+import Stats from "./Stats";
+
+const headerBackground = function(user) {
+  return {
+    background: `url('${user.profileBackgroundURL}') center no-repeat`
+  };
+};
+
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      navbarTab: "Stats",
+      user: {
+        avatarurl:
+          "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/57/573f45615e0fe0e8dcbcd835538fa404994f5529_full.jpg",
+        username: "Tacyarg | vgodogg.com",
+        profileBackgroundURL:
+          "https://media.giphy.com/media/KVZWZQoS0yqfIiTAKq/giphy.gif",
+        steamProfileURL: "https://steamcommunity.com/id/tacyarg2/",
+        steamid: "76561198403312375",
+        tradeURL: "https://trade.opskins.com/t/3667841/6eX2NLWh"
+      }
+    };
+  }
+
+  handleTabChange(navbarTab) {
+    return this.setState({ navbarTab });
+  }
+
+  render() {
+    var { user, navbarTab } = this.state;
+
+    return (
+      <div className="Profile-content">
+        <div className="Profile-content-header">
+          <img
+            className="Profile-content-header-avatar"
+            src={user.avatarurl}
+            alt={user.username}
+          />
+          <div className="Profile-content-header-userdetails">
+            <span className="Profile-content-header-username">
+              {user.username}
+            </span>
+            <span className="Profile-content-header-steamurl">
+              <b>SteamID:</b> {user.steamid}
+            </span>
+            {/* <span className="Profile-content-header-steamurl"><b>ProfileURL:</b> <a target="_Blank" href={user.steamProfileURL}>{user.steamProfileURL}</a></span> */}
+          </div>
+        </div>
+        <div className="Profile-content-body">
+          <Tabs
+            className="Profile-content-body-navbar"
+            large={true}
+            animate={true}
+            onChange={this.handleTabChange.bind(this)}
+            selectedTabId={navbarTab}
+          >
+            <Tab id="Stats" title="Stats" />
+            <Tab id="History" title="History" />
+            <Tab id="Settings" title="Settings" />
+          </Tabs>
+
+          <div className="Profile-content-body-panel-wrapper">
+            <Stats
+              aria-hidden={navbarTab == "Stats" ? false : true}
+              content="Stats"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Profile;
