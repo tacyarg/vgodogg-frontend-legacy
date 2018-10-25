dc4ec5141636484ba062ee6b3eb3fb91569af6b1
tacyarg <jdent@tacyarg.com>
2018-10-24 19:36:18 -0500

    dark loader

--- src/pages/Loading/Loading.js
+++ src/pages/Loading/Loading.js
@@ -6,7 +6,9 @@ import { Spinner } from "@blueprintjs/core";
 class Loading extends Component {
   render() {
     return (
-      <div className="Loading-main">
+      <div className="Loading-main"
+      style={{ color: 'white', background: '#10161A' }}
+      >
         <div className="Loading-loader">
           <Spinner />
           <h2>Preparing your unboxing experience...</h2>
